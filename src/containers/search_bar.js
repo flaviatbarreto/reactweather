a9cd4dd6ba0038698c1f376b0fc1f516d30864f3
Flávia Barreto <flavia.t.barreto@gmail.com>
2018-05-17 21:42:08 -0300

    add events to search bar

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,11 +1,33 @@
 import React, { Component } from 'react';
 
 export default class SearchBar extends Component {
-    render () {
+    constructor(props) {
+        super(props);
+        this.state = { term: '' };
+        this.onInputChange = this.onInputChange.bind(this);
+        this.onFormSubmit = this.onFormSubmit.bind(this);
+    }
+
+    onInputChange(event) {
+        this.setState({ term: event.target.value });
+    }
+
+    onFormSubmit(event) {
+        event.preventDefault();
+    }
+
+    render() {
         return (
-            <form className="input-group">
-                <input />
-                <button type="submit" className="btn btn-primary">Submit</button>
+            <form onSubmit={this.onFormSubmit} className="input-group">
+                <input
+                    placeholder="Get a five-day forecast in your favorite cities"
+                    className="form-control"
+                    value={this.state.term}
+                    onChange={this.onInputChange}
+                />
+                <span className="input-group-btn">
+                    <button type="submit" className="btn btn-primary">Submit</button>
+                </span>
             </form>
         );
     }
