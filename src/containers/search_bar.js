a0151cd698d8ad899c561739b15fc4c7783b4f4e
Flávia Barreto <flavia.t.barreto@gmail.com>
2018-05-17 21:22:46 -0300

    add search bar

---
+++ src/containers/search_bar.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class SearchBar extends Component {
+    render () {
+        return (
+            <form className="input-group">
+                <input />
+                <button type="submit" className="btn btn-primary">Submit</button>
+            </form>
+        );
+    }
+}
